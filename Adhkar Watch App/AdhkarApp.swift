1c08cdbbcd822c8730ada5e5e740c1f53a765241
Mohamad Arnaout <mohamadarnaout@gmail.com>
2025-05-01 14:29:02 -0400

    Initial Commit

---
+++ Adhkar Watch App/AdhkarApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdhkarApp.swift
+//  Adhkar Watch App
+//
+//  Created by Mohamad Arnaout on 2025-05-01.
+//
+
+import SwiftUI
+
+@main
+struct Adhkar_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Adhkar Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Adhkar Watch App
+//
+//  Created by Mohamad Arnaout on 2025-05-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
